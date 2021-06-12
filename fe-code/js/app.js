a85a2a76ae8edd9c4de677e943a02aa651cec481
VickyManora <vickymanora@gmail.com>
2021-06-12 11:30:48 +0530

    code refactoring

--- fe-code/js/app.js
+++ fe-code/js/app.js
@@ -1,5 +1,6 @@
 var emailBtn=false,mobileBtn=false,$phone=document.getElementById("phone"),$email=document.getElementById("email"),
-$phoneBtn= document.getElementById("phone-btn"), $emailBtn=document.getElementById("email-btn"); 
+$phoneBtn= document.getElementById("phone-btn"), $emailBtn=document.getElementById("email-btn"),
+$loaderContainer=document.getElementById("loader-container"),$section1= document.getElementById("section1"),$section2= document.getElementById("section2"); 
 function removeErorrCss(){
   document.querySelector('#email').parentNode.classList.remove("email-error");
   document.querySelector('#phone').parentNode.classList.remove("phone-error");
@@ -8,9 +9,9 @@ function emailClicked() {
   removeErorrCss();
   localStorage.clear();
   $phone.style.display="none";
- $email.style.display="block";
- $phoneBtn.classList.remove("clicked");
- $emailBtn.classList.add("clicked");
+  $email.style.display="block";
+  $phoneBtn.classList.remove("clicked");
+  $emailBtn.classList.add("clicked");
   emailBtn=true;
   mobileBtn=false;
   
@@ -25,9 +26,11 @@ function phoneClicked() {
   emailBtn=false;
   mobileBtn=true;
 }
-
-$(document).ready(function () {
-  document.getElementById("loader-container").style.display = "none";
+function hidesection(){
+ $section1.classList.add("sectionhide");
+ $section2.classList.add("sectiondisplaynone");
+}
+$loaderContainer.style.display = "none";
  $emailBtn.classList.add("clicked");
  $email.style.display="block";
   $phone.style.display="none";
@@ -37,7 +40,6 @@ $(document).ready(function () {
     localStorage.clear(); //Clears storage for next request
     if(emailBtn){
       email = $('#email').val().toLowerCase();
-
       var isEmailValidated;
       regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
       if (email.match(regEx)) {
@@ -48,13 +50,15 @@ $(document).ready(function () {
   
       if (isEmailValidated === true) {
         removeErorrCss();
-  document.getElementById("loader-container").style.display = "block";
+        hidesection();
+        $loaderContainer.style.display = "block";
         const proxyurl = "";
         const url =
           'https://ltv-data-api.herokuapp.com/api/v1/records.json?email=' + email;
         fetch(proxyurl + url)
           .then((response) => {
-  document.getElementById("loader-container").style.display = "none";
+            
+         $loaderContainer.style.display = "none";
             return response.text()})
           .then(function (contents) {
             localStorage.setItem("userObject", contents);
@@ -67,7 +71,6 @@ $(document).ready(function () {
     }
     else if(mobileBtn){
       phone = $('#phone').val().toLowerCase();
-
       var isPhoneValidated;
       regEx =  /^\d{10}$/;
       if (phone.match(regEx)) {
@@ -77,14 +80,15 @@ $(document).ready(function () {
       }
   
       if (isPhoneValidated === true) {
-  document.getElementById("loader-container").style.display = "block";
+        hidesection();
+        document.getElementById("loader-container").style.display = "block";
         removeErorrCss();
         const proxyurl = "";
         const url =
           'https://ltv-data-api.herokuapp.com/api/v1/records.json?phone=' + phone;
         fetch(proxyurl + url)
           .then((response) => {
-  document.getElementById("loader-container").style.display = "none";
+          document.getElementById("loader-container").style.display = "none";
             return response.text()})
           .then(function (contents) {
             localStorage.setItem("userObject", contents);
@@ -136,4 +140,4 @@ $(document).ready(function () {
   //   }
   // });
 
-});
+
